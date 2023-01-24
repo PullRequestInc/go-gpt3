136b50e3a2f8af132b7bffcfaa05a5203cd7edbc
Aidan Holland <aidan@furl.ai>
2023-01-23 20:07:51 -0500

    chore: boilerplate tests

--- client_test.go
+++ client_test.go
@@ -1,11 +1,9 @@
 package gpt3_test
 
 import (
-	"net/http"
 	"testing"
 
 	"github.com/PullRequestInc/go-gpt3"
-	fakes "github.com/PullRequestInc/go-gpt3/go-gpt3fakes"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,11 +11,4 @@ func TestInitNewClient(t *testing.T) {
 	client, err := gpt3.NewClient("test-key")
 	assert.Nil(t, err)
 	assert.NotNil(t, client)
-}
-
-func fakeHttpClient() (*fakes.FakeRoundTripper, *http.Client) {
-	rt := &fakes.FakeRoundTripper{}
-	return rt, &http.Client{
-		Transport: rt,
-	}
 }
---
+++ edits_test.go
@@ -0,0 +1,1 @@
+package gpt3_test
